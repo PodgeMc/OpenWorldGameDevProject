ee1dab699a961657042d8c94b8c7f80f24fa36f6
PodgeMc <padraig.a.d.mccormack@outlook.com>
2024-10-11 10:38:10 +0100

    Initial Push

---
+++ Assets/Scripts/TimeTravelManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TimeTravelManager : MonoBehaviour
+{
+
+    public int currentLevel;
+    public int score;
+    //public Path path;
+    private double version;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
